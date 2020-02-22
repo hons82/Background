755931fd15ab8f6838a59d308a96102f107d10c1
Hannes Tribus <hannes@vertical-life.info>
2020-02-22 08:01:08 +0100

    Initial Commit

---
+++ Background/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Background
+//
+//  Created by Hannes Tribus on 22/02/2020.
+//  Copyright © 2020 3Bus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
